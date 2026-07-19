a87b8b591eed5522543a830d46e89e5a64a10511
OPT Dulanjana <140092772+TheekshanaDulanjana@users.noreply.github.com>
2026-07-19 15:26:25 +0530

    Weding Image add Signed-off-by: OPT Dulanjana <140092772+TheekshanaDulanjana@users.noreply.github.com>

--- src/Components/WhatsappButton.jsx
+++ src/Components/WhatsappButton.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import whatsappDP from "../assets/WhatsappDP.png";
+import WABD from "../assets/WABD.png";
 
 const WhatsappWidget = ({ showScrollButton }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,7 +20,7 @@ const WhatsappWidget = ({ showScrollButton }) => {
 
   return (
     <div
-      className={`fixed right-4 md:right-10 z-50 flex flex-col items-end transition-all duration-300 ease-in-out ${
+      className={`fixed right-4 md:right-10 z-50  flex flex-col items-end transition-all duration-300 ease-in-out ${
         showScrollButton
           ? "bottom-[5.8rem] md:bottom-[6.2rem]"
           : "bottom-6 md:bottom-8"
@@ -27,46 +28,55 @@ const WhatsappWidget = ({ showScrollButton }) => {
     >
       {/* Chat Popup */}
       <div
-        className={`mb-4 transition-all duration-300 ease-in-out origin-bottom-right ${
+        className={`mb-4 transition-all  duration-300 ease-in-out origin-bottom-right ${
           isOpen
             ? "opacity-100 scale-100 translate-y-0 pointer-events-auto"
             : "opacity-0 scale-95 translate-y-4 pointer-events-none"
         }`}
       >
-        <div className="p-6 bg-black bg-gradient-to-tr from-white/15 to-transparent rounded-2xl shadow-2xl w-[90vw] md:w-96 border border-white/50 backdrop-blur-md">
-          <div className="flex items-center gap-3 mb-5">
-            <img
-              src={whatsappDP}
-              alt="Studio VisualFX"
-              className="w-10 h-10 rounded-full object-cover"
-            />
+        <div
+          style={{
+            backgroundImage: `url(${WABD})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          className="p-6 rounded-2xl shadow-lg shadow-black/30  w-[90vw] md:w-96 backdrop-blur-md overflow-hidden border border-white/20"
+        >
+          {/* Header Section */}
+          <div className="-mx-6 -mt-6 mb-6 rounded-t-[2xl] bg-[#161717] px-6 py-3 border-b border-white/5 shadow-lg">
+            <div className="flex items-center gap-3">
+              <img
+                src={whatsappDP}
+                alt="Studio VisualFX"
+                className="w-10 h-10 rounded-full object-cover"
+              />
 
-            <div>
-              <p className="text-white font-roboto text-base">
-                Studio VisualFX (PVT) Ltd
-              </p>
+              <div>
+                <p className="text-white font-roboto tracking-wide text-sm">
+                  Studio VisualFX (PVT) Ltd
+                </p>
 
-              <p className="text-xs font-roboto text-green-400 flex items-center gap-1.5">
-                <span className="relative flex h-2 w-2">
-                  <span className="absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75 animate-ping"></span>
-                  <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
-                </span>
-                Online
-              </p>
+                <p className="text-xs font-roboto text-[#21C063] flex items-center">
+                  
+                  Online
+                </p>
+              </div>
             </div>
           </div>
 
-          <div className="relative mb-5 rounded-xl bg-[#161717] p-4 text-sm leading-relaxed text-white">
-            <p className="mb-1 font-semibold text-green-300">Hi 👋</p>
+          {/* Message Section */}
+          <div className="relative mb-5 rounded-xl bg-[#161717] shadow-md shadow-black/30 border-l-2 border-[#21C063] p-4 text-sm leading-relaxed text-white">
+            <p className="mb-1 font-semibold text-[#21C063]">Hi 👋</p>
 
             Welcome to Studio VisualFX.
             <br />
             How can we help you today?
           </div>
 
+          {/* WhatsApp Button */}
           <button
             onClick={openWhatsApp}
-            className="flex w-full cursor-pointer items-center justify-center gap-2.5 rounded-xl bg-green-600 py-3 font-roboto text-white shadow-md transition-all duration-300 hover:bg-green-700"
+            className="flex w-full cursor-pointer shadow-md shadow-black/30 items-center justify-center gap-2.5 rounded-xl bg-[#21C063] py-3 font-roboto text-white  transition-all duration-300 hover:bg-green-700"
           >
             <FaWhatsapp className="text-xl" />
             Start Chat
